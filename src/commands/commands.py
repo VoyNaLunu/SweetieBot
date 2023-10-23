ed3f762e3c5b24b42d7340e514f8d39643ef5923
VoyNaLunu <lalalalaskir@gmail.com>
2023-10-23 16:56:49 +0300

    change the way commands.yaml is loaded

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -6,19 +6,24 @@ from discord.ext import commands
 
 WORKDIR = os.getcwd()
 
+def load_commands() -> dict:
+    """returns dictionary of commands and their descriptions"""
+    with open(f'{WORKDIR}/src/commands/commands.yaml', encoding="UTF-8") as data:
+        return yaml.load(data, Loader=yaml.loader.SafeLoader)
+
+COMMAND_LIST = load_commands()
+
 class UtilCommands(commands.Cog):
     def __init__(self, bot: discord.Bot) -> None:
         self.bot = bot
 
-    @commands.slash_command(description="shows all available commands")
+    @commands.slash_command(description=COMMAND_LIST['help']['description'])
     async def help(self, ctx):
         """returns list of commands"""
         embed = discord.Embed(
             title="Hello I am SweetieBot, here are my commands:",
             color=discord.Colour.purple()
         )
-        with open(f'{WORKDIR}/src/commands/commands.yaml') as data:
-            command_list = yaml.load(data, Loader=yaml.loader.SafeLoader)
-        for command in command_list.keys():
-            embed.add_field(name=command_list[command]["name"], value=command_list[command]["description"], inline=False)
+        for command in COMMAND_LIST.keys():
+            embed.add_field(name=COMMAND_LIST[command]["name"], value=COMMAND_LIST[command]["description"], inline=False)
         await ctx.respond(embed=embed, ephemeral=True)
