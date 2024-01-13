828177d87d8eb747ecc13d1948abe15d8c6cb977
VoyNaLunu <lalalalaskir@gmail.com>
2024-01-14 01:31:22 +0300

    add error handling do derpibooru command

--- src/bot/commands/commands.py
+++ src/bot/commands/commands.py
@@ -6,7 +6,7 @@ import yaml
 import discord
 from discord.ext import commands
 import philomena
-
+from . import exceptions
 
 WORKDIR = os.getcwd()
 IMG_REGEX = r"(.*)(\.png|.jpg|.jpeg|.gif)"
@@ -77,60 +77,63 @@ class DerpibooruCommands(commands.Cog):
     async def derpibooru(self, ctx, tags, filter_id=""):
         await ctx.defer()
 
-        images = self.board.random_image(tags, filter_id=filter_id)
-        embed = None
-        image = None
-        uploader_name = None
-        uploader_avatar = None
-        image_upvotes = None
-        image_downvotes = None
+        try:
+            images = self.board.random_image(tags, filter_id=filter_id)
+            embed = None
+            image = None
+            uploader_name = None
+            uploader_avatar = None
+            image_upvotes = None
+            image_downvotes = None
 
-        if not images:
-            message = f"I couldn't find anything with tags: `{tags}` :("
-            await ctx.followup.send(message)
-            return
-        if "error_message" in images:
-            logger.log(
-                logger.ERROR, msg=f"following error occured when requesting images: \n {images}")
+            if not images:
+                message = f"I couldn't find anything with tags: `{tags}` :("
+                await ctx.followup.send(message)
+                return
+            if "error_message" in images:
+                raise exceptions.ImageFetchFailed(
+                    f"failed to fetch image, server response: {images}")
+            image = images["images"][0]
+            if image["uploader_id"]:
+                uploader_id = image["uploader_id"]
+                uploader_name = image["uploader"]
+                uploader = self.board.profile(uploader_id)
+                uploader_avatar = uploader["user"]["avatar_url"]
+            message = f"I found this using tags: `{tags}`"
+            title = "Derpibooru"
+            description = image["description"]
+            image_url = image["view_url"]
+            post_url = f'{self.board.base_url}/images/{image["id"]}/'
+            image_upvotes = image["upvotes"]
+            image_downvotes = image["downvotes"]
+            image_faves = image["faves"]
+            image_created_at = datetime.strptime(
+                image["created_at"], '%Y-%m-%dT%H:%M:%S%z')
+            if re.match(IMG_REGEX, image_url):
+                embed = embed_image(
+                    title=title,
+                    description=description,
+                    image_url=image_url,
+                    post_url=post_url,
+                    author=uploader_name,
+                    author_avatar=uploader_avatar,
+                    image_upvotes=image_upvotes,
+                    image_downvotes=image_downvotes,
+                    image_faves=image_faves,
+                    created_at=image_created_at,
+                )
+            if re.match(VIDEO_REGEX, image_url):
+                uploader_name = f'** **\n**Uploaded by:**\n**{uploader_name}**\n** **'
+                video_description = f'**Description:**\n{description}'
+                if description:
+                    # doing it this way so there's no extra padding
+                    video_description = f'{video_description}\n** **'
+                post_url = f'**Link to the post:**\n{post_url}\n** **'
+                stats = f'⬆️ {image_upvotes} ⬇️ {image_downvotes} ⭐ {image_faves} **•** {image_created_at.strftime("%d/%m/%Y %H:%M")}\n** **'
+                message = f'{message}\n{uploader_name}\n{video_description}\n{post_url}\n{stats}'
+            await ctx.followup.send(message, embed=embed)
+        except Exception as error:
             message = "sorry something went wrong"
-            await ctx.followup.send(message)
-            return
-        image = images["images"][0]
-        if image["uploader_id"]:
-            uploader_id = image["uploader_id"]
-            uploader_name = image["uploader"]
-            uploader = self.board.profile(uploader_id)
-            uploader_avatar = uploader["user"]["avatar_url"]
-        message = f"I found this using tags: `{tags}`"
-        title = "Derpibooru"
-        description = image["description"]
-        image_url = image["view_url"]
-        post_url = f'{self.board.base_url}/images/{image["id"]}/'
-        image_upvotes = image["upvotes"]
-        image_downvotes = image["downvotes"]
-        image_faves = image["faves"]
-        image_created_at = datetime.strptime(
-            image["created_at"], '%Y-%m-%dT%H:%M:%S%z')
-        if re.match(IMG_REGEX, image_url):
-            embed = embed_image(
-                title=title,
-                description=description,
-                image_url=image_url,
-                post_url=post_url,
-                author=uploader_name,
-                author_avatar=uploader_avatar,
-                image_upvotes=image_upvotes,
-                image_downvotes=image_downvotes,
-                image_faves=image_faves,
-                created_at=image_created_at,
-            )
-        if re.match(VIDEO_REGEX, image_url):
-            uploader_name = f'** **\n**Uploaded by:**\n**{uploader_name}**\n** **'
-            video_description = f'**Description:**\n{description}'
-            if description:
-                # doing it this way so there's no extra padding
-                video_description = f'{video_description}\n** **'
-            post_url = f'**Link to the post:**\n{post_url}\n** **'
-            stats = f'⬆️ {image_upvotes} ⬇️ {image_downvotes} ⭐ {image_faves} **•** {image_created_at.strftime("%d/%m/%Y %H:%M")}\n** **'
-            message = f'{message}\n{uploader_name}\n{video_description}\n{post_url}\n{stats}'
-        await ctx.followup.send(message, embed=embed)
+            logger.log(
+                logger.ERROR, f"following error occured in derpibooru command: {error}")
+            await ctx.followup.send("sorry something went wrong")
